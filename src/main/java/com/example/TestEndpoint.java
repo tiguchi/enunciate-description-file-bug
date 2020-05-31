1cdfe2e6610ec3c6d8588bc57b4805948b386c07
Thomas Iguchi <ti@nobu-games.com>
2020-05-31 10:20:52 -0700

    Initial commit

---
+++ src/main/java/com/example/TestEndpoint.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("test")
+public class TestEndpoint {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getTest() {
+        return "Hello World";
+    }
+}
